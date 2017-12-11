42bddfe314661114107fd4fba26686d654015057
Tyler Chi <itychi@gmail.com>
2017-12-10 17:25:13 -0800

    removed ignore

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+//dev.js - don't commit this.
+
+module.exports = {
+  googleClientID:
+    "1087115872945-uo3bg24g2694j462r67e0k5mnau7tte8.apps.googleusercontent.com",
+  googleClientSecret: "iuxp6aUhvyxOdyHWhAhsYfKP",
+  mongoURI: "mongodb://fpuser:fppassword@ds133876.mlab.com:33876/flexproject",
+  cookieKey: "iaweofijawofeijawoefijawoefijaoi"
+};
