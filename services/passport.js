2140b9df88d60a3c431d699335f9cfef2ea4f81b
Tyler Chi <itychi@gmail.com>
2017-12-10 16:08:24 -0800

    tweak google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -25,7 +25,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       //first, we need to check if the googleId exists in the record though
