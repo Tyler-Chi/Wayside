95534312a79620bda2a017107f857bcde37e28ef
Tyler Chi <itychi@gmail.com>
2017-12-11 21:46:40 -0800

    did the components

---
+++ client/src/components/CustomersOrdersNew.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class CustomersOrdersNew extends Component {
+  render() {
+    return <div>I AM CUSTOMERS ORDERS NEW</div>;
+  }
+}
+
+export default connect()(CustomersOrdersNew);
