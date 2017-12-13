48d7c8e5d552afe274e72c6c5889ea7b28591f6f
Tyler Chi <itychi@gmail.com>
2017-12-12 21:16:19 -0800

    stuff

--- client/src/components/Drivers/DriversTripsHistory.js
+++ client/src/components/Drivers/DriversTripsHistory.js
@@ -1,10 +1,43 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import * as actions from "../../actions";
 
 class DriversTripsHistory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    };
+  }
+
+  //need to figure out the promises here -__-
+  componentWillMount() {
+    this.props.fetchCompletedTrips();
+    setTimeout(() => this.setState({ loading: false }), 2000);
+  }
+
   render() {
-    return <div>Hello world!</div>;
+    if (this.state.loading) {
+      return <div>loading</div>;
+    }
+
+    const { trips } = this.props.entities;
+    console.log("trips", trips);
+
+    return (
+      <div className="drivers-trips-history-index-area">
+        <p className="past-deliveries">Past Deliveries</p>
+        {trips.map(trip => <p> hi </p>)}
+      </div>
+    );
   }
 }
 
-export default connect()(DriversTripsHistory);
+function mapStateToProps({ auth, entities }) {
+  return {
+    auth,
+    entities
+  };
+}
+
+export default connect(mapStateToProps, actions)(DriversTripsHistory);
