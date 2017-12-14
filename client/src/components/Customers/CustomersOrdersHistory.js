071e2e51321b0e5b00cc09874cb4cac6592b8e35
Tyler Chi <itychi@gmail.com>
2017-12-13 21:53:51 -0800

    put fetchOrders on the customer orders history

--- client/src/components/Customers/CustomersOrdersHistory.js
+++ client/src/components/Customers/CustomersOrdersHistory.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import * as actions from "../../actions";
 
 class CustomersOrdersHistory extends Component {
+  componentWillMount() {
+    this.props.fetchOrders();
+  }
+
   render() {
     return <div>I AM CUSTOMERS ORDERS HISTORY</div>;
   }
 }
 
-export default connect()(CustomersOrdersHistory);
+export default connect(null, actions)(CustomersOrdersHistory);
