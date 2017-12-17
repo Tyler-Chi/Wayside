16edec17314843f4cceb35751bafecc669c204fe
Tyler Chi <itychi@gmail.com>
2017-12-16 19:10:03 -0800

    heroku pls

--- index.js
+++ index.js
@@ -37,5 +37,14 @@ require("./routes/authRoutes")(app);
 require("./routes/tripRoutes")(app);
 require("./routes/orderRoutes")(app);
 
+if (process.env.NODE_ENV === 'production'){
+  //first makes sure that express will serve up production assets
+  app.use(express.static('client/build'));
+
+  app.get('*',(req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
