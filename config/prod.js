93ef44591a19401563884b5aff0594f2580fac4c
Tyler Chi <itychi@gmail.com>
2017-12-17 20:54:24 -0800

    stuff

--- config/prod.js
+++ config/prod.js
@@ -1,16 +1,8 @@
 //prod.js - production keys here!!!
 
-// module.exports = {
-//   googleClientID: process.env.GOOGLE_CLIENT_ID,
-//   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//   mongoURI: process.env.MONGO_URI,
-//   cookieKey: process.env.COOKIE_KEY
-// };
-
 module.exports = {
-  googleClientID:
-    "1087115872945-uo3bg24g2694j462r67e0k5mnau7tte8.apps.googleusercontent.com",
-  googleClientSecret: "iuxp6aUhvyxOdyHWhAhsYfKP",
-  mongoURI: "mongodb://fpuser:fppassword@ds133876.mlab.com:33876/flexproject",
-  cookieKey: "iaweofijawofeijawoefijawoefijaoi"
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
 };
